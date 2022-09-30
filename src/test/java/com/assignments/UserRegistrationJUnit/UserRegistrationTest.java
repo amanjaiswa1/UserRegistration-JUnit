855074679dcff73d948751077e21319571d21dda
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-30 11:48:21 +0530

    Add: JUnit Parametrised Test To Validate Multiple Entry For The Email Address

--- src/test/java/com/assignments/UserRegistrationJUnit/UserRegistrationTest.java
+++ src/test/java/com/assignments/UserRegistrationJUnit/UserRegistrationTest.java
@@ -1,20 +1,22 @@
 package com.assignments.UserRegistrationJUnit;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
 	@Test
 	public void givenCorrectDetailsReturnHappy() {
 		UserRegistration validate = new UserRegistration();
-		String result = validate.userRegistrationValidation("Tekesh", "Singh", "tekesh.singh@gmail.co.in", "91 1234567890", "Tekesh1Singh@");
-		Assert.assertEquals("Happy", result);
+		String result = validate.userRegistrationValidation("Tekesh", "Singh", "tekesh.singh@gmail.co.in",
+				"91 1234567890", "Tekesh1Singh@");
+		assertEquals("Happy", result);
 	}
 
 	@Test
 	public void givenIncorrectDetailsReturnSad() {
 		UserRegistration validate = new UserRegistration();
-		String result = validate.userRegistrationValidation("tekesh", "singh", "tekesh-Singh@gmail.co.in1", "919087654321", "Tekesh@");
-		Assert.assertEquals("Sad", result);
+		String result = validate.userRegistrationValidation("tekesh", "singh", "tekesh-Singh@gmail.co.in1",
+				"919087654321", "Tekesh@");
+		assertEquals("Sad", result);
 	}
 }
